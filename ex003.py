f67584b5e2919ebed0180ef4444414ac58f4a53b
IZAQUE DOS SANTOS MACHADO <machadoizaquedossantos@gmail.com>
2026-04-05 10:37:35 -0300

    adicionando exercício 003

---
+++ ex003.py
@@ -0,0 +1,11 @@
+# Exercício 003 - Curso em Vídeo - Operadores Aritméticos
+n1=int(input('digite algum numero'))
+n2=int(input('digite outro numero'))
+soma=n1+n2
+sub=n1-n2
+mult=n1*n2
+div=n1/n2
+pot=n1**n2
+divin=n1//n2
+rdiv=n1%n2
+print('soma = {}\nsubtração = {}\nmultiplicação = {}\ndivisão = {}\npotencia = {}\ndivisão inteira = {}\nresto da divisão = {}'.format(soma,sub,mult,div,pot,divin,rdiv))
