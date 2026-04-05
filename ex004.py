2c78eb6109de4740daf420cb39017db4536e8cc9
IZAQUE DOS SANTOS MACHADO <machadoizaquedossantos@gmail.com>
2026-04-05 10:41:51 -0300

    adicionando exercício 004

---
+++ ex004.py
@@ -0,0 +1,5 @@
+# Exercício 004 - Curso em Vídeo - Antecessor e sucessor
+n1: int=int(input('Digite um numero: '))
+ant=n1-1
+suc=n1+1
+print('O numero antecessor é {}\no numero sucessor é {}'.format(ant,suc))
